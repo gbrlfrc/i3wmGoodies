b45e7433bc722cedd08a90443adcb76b16db14b1
gabriele <61314427+gbrlfrc@users.noreply.github.com>
2021-04-18 12:33:48 +0000

    Add files via upload

--- network.py
+++ network.py
@@ -20,14 +20,14 @@ def get_argv():
         if(sys.argv[1]=='--new' or sys.argv[1] == '-n'): return 1
         elif(sys.argv[1]=='--connect' or sys.argv[1]== '-c'): return 2
         else: return -1
-    except:
+    except IOError:
         sys.exit('argument required')
 
 def helper():
     print('syntax: command arg')
     print('arg: ')
-    print('\t --connect, -c : scan for nerby wifi networks and connect to it (wifi network interface with: ip link)')
-    print('\t --new, -n : enable network interface and connect')
+    print('\t --connect, -c : enable network interface and connect')
+    print('\t --new, -n : scan for nerby wifi networks and connect to it (wifi network interface with: ip link)')
 
 def build_table(networks):
     ssid=[]
@@ -46,7 +46,7 @@ def build_table(networks):
     net={'ssid': ssid, 'MAC_address': address, 'signal': signal, 'quality': quality, 'channel': channel, 'mode': mode}
     return net
 
-def new_network(interface):
+def new_network(interface, config_file):
     networks=wifi.Cell.all(interface)
     net=build_table(networks)
 
@@ -54,14 +54,24 @@ def new_network(interface):
     selected_network=input('select network [0, 1, 2, ...]: ')
     try:
         print(net['ssid'][int(selected_network)])
-        os.system('wpa_passphrase "'+ str(net['ssid'][int(selected_network)]) +'" '+ str(get_pssw()) +' >> /etc/wpa_supplicant/wpa_supplicant-wlp1s0.conf')
+        os.system('wpa_passphrase "'+ str(net['ssid'][int(selected_network)]) +'" '+ str(get_pssw()) +' >> '+config_file)
         connect_network(interface)
-    except:
+    except IOError:
         print('select valid network')
 
-def connect_network(interface):
+def connect_network(interface, config_file):
     os.system('dhcpcd')
-    os.system('wpa_supplicant -B -i '+ interface +' -c /etc/wpa_supplicant/wpa_supplicant-'+interface+'.conf')
+    os.system('wpa_supplicant -B -i '+ interface +' -c'+config_file)
+
+def check_config_file(interface):
+    config_file='/etc/wpa_supplicant/wpa_supplicant-'+str(interface)+'.conf'
+    if (not os.path.exists(config_file)): os.mknod(config_file, mode=0o0644)
+    return config_file
+
+def setup():
+    interface=get_interface()
+    config_file=check_config_file(interface)
+    return interface, config_file
 
 def main():
     check_root()
@@ -69,11 +79,11 @@ def main():
     if(get_argv()==0): helper()
     else:
         if(get_argv()==1):
-            interface=get_interface()
-            new_network(interface)
+            interface, config_file=setup()
+            new_network(interface, config_file)
         elif(get_argv()==2):
-            interface=get_interface()
-            connect_network(interface)
+            interface, config_file=setup()
+            connect_network(interface, config_file)
         else: sys.exit('invalid argument')
 
 if __name__=='__main__':
